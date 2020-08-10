99d5b636e4f6edd1337ca86d433a3cbaf99a4700
Jukka <jukka1993@foxmail.com>
2020-08-11 00:54:20 +0800

    new prj

---
+++ echoPrj/client/EchoClient/Assets/Scripts/Echo.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net.Sockets;
+using UnityEngine.UI;
+using System;
+
+public class Echo : MonoBehaviour {
+    Socket socket;
+    public InputField InputField;
+    public Text text;
+    public GameObject textParent;
+
+    byte[] readBuff = new byte[1024];
+    string recvStr = "";
+
+    public void Connection(){
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.BeginConnect("127.0.0.1", 8888,ConnectCallback,socket);
+    }
+    public void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            socket.EndConnect(ar);
+            Debug.Log("Socket Connect Succ");
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch(SocketException ex)
+        {
+            Debug.Log("Socket Connect fail" + ex.ToString());
+        }
+    }
+    public void ReceiveCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            int count = socket.EndReceive(ar);
+            recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+            socket.BeginReceive(readBuff, 0, 1024, 0, ReceiveCallback, socket);
+        }
+        catch (SocketException ex){
+            Debug.Log("Socket Receive fail" + ex.ToString());
+        }
+    }
+    public void Send()
+    {
+        string msg = InputField.text;
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(msg);
+        //socket.Send(sendBytes);
+        socket.BeginSend(sendBytes, 0, sendBytes.Length, 0, SendCallback, socket);
+
+        //byte[] readBuff = new byte[1024];
+        //int count = socket.Receive(readBuff);
+        //string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+        //text.text = recvStr;
+        //socket.Close();
+    }
+    public void SendCallback(IAsyncResult ar)
+    {
+        try
+        {
+            Socket socket = (Socket)ar.AsyncState;
+            int count = socket.EndSend(ar);
+            Debug.Log("Socket send succ" + count);
+        }
+        catch(SocketException ex)
+        {
+            Debug.Log("Socket Send fail" + ex.ToString());
+        }
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(recvStr != "")
+        {
+            Text newText = Instantiate(this.text);
+            newText.transform.parent = textParent.transform;
+            newText.text = recvStr;
+            Debug.Log("recvStr = " + recvStr);
+            recvStr = "";
+        }
+	}
+}
---
+++ echoPrj/server/EchoPrjServer/EchoPrjServer/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading;
+namespace EchoPrjServer
+{
+    class Program
+    {
+        static Dictionary<Socket, ClientState> clients = new Dictionary<Socket, ClientState>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HelloWorld");
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+            socket.Bind(ipEp);
+            socket.Listen(0);
+            Console.WriteLine("服务器启动成功...");
+            socket.BeginAccept(AcceptCallback, socket);
+            Console.ReadLine();
+            
+            //Socket connfd = socket.Accept();
+            //Console.WriteLine("[服务器]Accept");
+            //while (true)
+            //{
+            //    byte[] readBuff = new byte[1024];
+            //    int count = connfd.Receive(readBuff);
+            //    string readStr = System.Text.Encoding.Default.GetString(readBuff);
+            //    Console.WriteLine("[服务器接收]" + readStr);
+            //    byte[] sendBytes = System.Text.Encoding.Default.GetBytes(readStr);
+            //    connfd.Send(sendBytes);
+            //}
+        }
+        public static void AcceptCallback(IAsyncResult ar)
+        {
+            try
+            {
+                Console.WriteLine("[server] accept");
+                Socket listenfd = (Socket)ar.AsyncState;
+                Socket clientfd = listenfd.EndAccept(ar);
+
+                ClientState state = new ClientState();
+                state.socket = clientfd;
+                clients.Add(clientfd, state);
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+                listenfd.BeginAccept(AcceptCallback, listenfd);
+                //clients.Keys.GetEnumerator().MoveNext();
+                //var cur = clients.Keys.GetEnumerator().Current;
+                //Console.WriteLine("cur1");
+                //Console.WriteLine(cur);
+                //Console.WriteLine("cur2");
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("Socket Accept fail" + ex.ToString());
+            }
+        }
+        public static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                ClientState state = (ClientState)ar.AsyncState;
+                Socket clientfd = state.socket;
+                int count = clientfd.EndReceive(ar);
+                if (count == 0)
+                {
+                    clientfd.Close();
+                    clients.Remove(clientfd);
+                    Console.WriteLine("Socket Close");
+                    return;
+                }
+                string recvStr = System.Text.Encoding.Default.GetString(state.readBuff, 0, count);
+                byte[] sendBytes =
+                    System.Text.Encoding.Default.GetBytes("echo " + recvStr);
+                foreach(KeyValuePair<Socket, ClientState> kvp in clients)
+                {
+                    kvp.Key.Send(sendBytes);
+                }
+                //clientfd.Send(sendBytes);
+                //ClientState clientCur = new ClientState();
+                //clients.Keys.GetEnumerator().MoveNext();
+                //var cur = clients.Keys.GetEnumerator().Current;
+                //var cur = clients.Keys.GetEnumerator().Current;
+                //Console.WriteLine("cur11");
+                //Console.WriteLine(cur.ToString());
+                //Console.WriteLine("cur22");
+                //clients.TryGetValue(cur, out clientCur);
+                //clientCur.socket.Send(sendBytes);
+                //while (clients.Keys.GetEnumerator().MoveNext())
+                //{
+                //    clients.TryGetValue(clients.Keys.GetEnumerator().Current, out clientCur);
+                //    clientCur.socket.Send(sendBytes);
+                //}
+                clientfd.BeginReceive(state.readBuff, 0, 1024, 0, ReceiveCallback, state);
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine("socket Receive fail" + ex.ToString());
+            }
+
+        }
+    }
+    class ClientState
+    {
+        public Socket socket;
+        public byte[] readBuff = new byte[1024];
+    }
+}
