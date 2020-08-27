7e69e2cce575ea2a8434dae68140bb8c00153803
朱快 <zhukuai@git.wjr.com>
2020-08-27 18:50:52 +0800

    server json library

--- general/server/general/general/Program.cs
+++ general/server/general/general/Program.cs
@@ -7,6 +7,26 @@ namespace general
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MsgMove msgMove = new MsgMove();
+            msgMove.x = 10;
+            msgMove.y = 11;
+            msgMove.z = 12;
+            byte[] bytes = MsgBase.Encode(msgMove);
+            string s = System.Text.Encoding.UTF8.GetString(bytes);
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            Console.WriteLine(s);
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+            MsgMove msgMove1 = (MsgMove)MsgBase.Decode("MsgMove", bytes,0,bytes.Length);
+            Console.WriteLine(msgMove1.protoName);
+            Console.WriteLine(msgMove1.x);
+            Console.WriteLine(msgMove1.y);
+            Console.WriteLine(msgMove1.z);
+            Console.WriteLine("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+
+
+
+
             Console.ReadKey();
         }
     }
